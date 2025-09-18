539c8ad230a57da35649b0738ddfe94f82166572
manutatum <jbmanuel16@gmail.com>
2025-09-18 18:38:02 +0200

    My Projects casi terminado, cambios en el ContactForm y en la pagina index

---
+++ src/data/projects/myProjects.data.ts
@@ -0,0 +1,32 @@
+import type { Project } from "@/interfaces/Project.interface";
+
+export const myProjects: Project[] = [
+    {
+        title: "Book Manager API",
+        description: "API para una gestión personal de tus libros, permite añadir, eliminar y editar libros que te hayas leído o que tengas pendientes o en proceso.",
+        image: "",
+        url: "https://github.com/manutatum/BookManagerSpring",
+        technologies: ["Java", "Spring Boot", "MySQL", "Hibernate", "Swagger", "OpenPDF", "Spring Security", "JWT"]
+    },
+    {
+        title: "Chat WebSockets",
+        description: "Aplicación de chat en tiempo real. El frontend está desarrollado con React + TypeScript y Tailwind CSS, mientras que el backend está implementado en Java.",
+        image: "",
+        url: ["https://github.com/manutatum/chatWebSocketsFrontend", "https://github.com/manutatum/chatWebSocketsBackend"],
+        technologies: ["Java", "Spring Boot", "PostgreSQL", "Hibernate", "React", "TypeScript", "Tailwind CSS"]
+    },
+    {
+        title: "Teslo Shop",
+        description: "Teslo Shop es una tienda de ropa inspirada en la experiencia de compra de la tienda oficial de Tesla. La aplicación está diseñada para ofrecer una experiencia de usuario moderna, fluida y minimalista.",
+        image: "",
+        url: "https://github.com/manutatum/Teslo-Shop",
+        technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Prisma", "PostgreSQL", "React", "Zustand", "Next Auth"]
+    },
+    {
+        title: "TrassTareas",
+        description: "TrassTareas es una aplicación móvil que permite a los usuarios gestionar sus tareas.",
+        image: "",
+        url: "https://github.com/manutatum/TrassTareas",
+        technologies: ["Java", "Android Studio", "MySQL", "SQLite", "Glide"]
+    }
+];
---
+++ src/interfaces/Project.interface.ts
@@ -0,0 +1,7 @@
+export interface Project {
+  title: string;
+  description: string;
+  image: string;
+  url: string | string[];
+  technologies: string[];
+}
