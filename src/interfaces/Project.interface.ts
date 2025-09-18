236c04671fb5080528e310f4d4d2f8900b706537
manutatum <jbmanuel16@gmail.com>
2025-09-19 01:22:44 +0200

    ProjectsCards terminados

--- src/interfaces/Project.interface.ts
+++ src/interfaces/Project.interface.ts
@@ -1,7 +1,9 @@
+import type { ImageMetadata } from "astro";
+
 export interface Project {
   title: string;
   description: string;
-  image: string;
-  url: string | string[];
+  image?: ImageMetadata;
+  url: string;
   technologies: string[];
 }
